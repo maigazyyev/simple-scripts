a12ef30728ae068e80e97d6d3b66bdb8ffde3440
t.maigazyyev <t.maigazyyev@mycar.kz>
2023-05-16 15:17:00 +0600

    check gitlab-server version and notify to slack

---
+++ check_gitlab_version.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+export str1=`yum list | grep gitlab-ee | awk {'print $2'} | awk '(NR == 1)'`
+export str2=`yum list | grep gitlab-ee | awk {'print $2'} | awk '(NR == 2)'`
+
+if [ "$str1" != "$str2" ]; then
+   curl -X POST -H 'Content-type: application/json' --data '{"text":"update Gitlab version to '$str2'"}' https://hooks.slack.com/services/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
+fi
+
+# checks every week in Sunday  if exists new version then sent notification to slack channel
+
+# crontab -e 
+
+# 0 19 * * SUN /root/gitlab_version_check.sh
